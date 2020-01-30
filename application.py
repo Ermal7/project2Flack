122c7275449faeb21882d0564a714745bb5b17eb
root <root@localhost.localdomain>
2020-01-30 10:16:12 -0500

    added create channel feature

--- application.py
+++ application.py
@@ -23,7 +23,7 @@ def index():
         if session.get("display_name") is None:
             return render_template("register.html")
         else:
-            return render_template("index.html", display_name=session["display_name"],display_names=display_names)
+            return render_template("index.html", display_name=session["display_name"],display_names=display_names,channel_names=channel_names)
 
     else:
         if session.get("display_name") is None:
@@ -31,4 +31,15 @@ def index():
             session["display_name"]= display_name
             display_names.append(display_name)
             #printf(display_names)
-        return render_template("index.html", display_name=session["display_name"],display_names=display_names)
+        return render_template("index.html", display_name=session["display_name"],display_names=display_names,channel_names=channel_names)
+
+
+
+@app.route("/create", methods=["POST"])
+def create_channel():
+    new_channel = request.form.get("channel_name")
+    for channel in channel_names:
+        if channel == new_channel:
+            return "exists"
+    channel_names.append(new_channel)
+    return new_channel
--- static/index.js
+++ static/index.js
@@ -1,46 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
+      // Template for adding new channel
+      const template = Handlebars.compile("<li>{{ channel }}</li>");
 
+      // Creating New Channel
+      document.querySelector('#channelform').onsubmit = () => {
+        // Initialize new request
+        const request = new XMLHttpRequest();
+        const channel_name = document.querySelector('#channel_name').value;
+        request.open('POST', '/create');
+        // Callback function for when request completes
+        request.onload = () => {
+            // Extract  from request
+            const new_channel = request.responseText;
+            const exists = "exists";
 
-  document.querySelector('#form1').onsubmit = () => {
-
-    // Initialize new request
-    const request = new XMLHttpRequest();
-    const d_name = document.querySelector('#display_name').value;
-    request.open('POST', '/');
-
-    // Callback function for when request completes
-    request.onload = () => {
-
-        // Extract  from request
-        const display_name = request.responseText;
-
-        // Update the result div
-        if (data.success) {
-            const contents = `1 EUR is equal to ${data.rate} ${currency}.`
-            document.querySelector('#result').innerHTML = contents;
+            if (new_channel != exists) {
+              const content = template({'channel': new_channel});
+              document.querySelector('#channels').innerHTML += content;
+            }
         }
-        else {
-            document.querySelector('#result').innerHTML = 'There was an error.';
-        }
-    }
-
-    // Add data to send with request
-    const data = new FormData();
-    data.append('display_name', d_name);
-
-    // Send request
-    request.send(data);
-    return false;
-};
+        // Add data to send with request
+        const data = new FormData();
+        data.append('channel_name', channel_name);
+        // Send request
+        request.send(data);
+        return false;
+    };
 
 
 
